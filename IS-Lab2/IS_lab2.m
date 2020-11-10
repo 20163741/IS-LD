6e243ac096d8ecdd2fe89ef5661e5da32bf8c399
20163741 <36729822+20163741@users.noreply.github.com>
2020-11-10 14:57:10 +0200

    Add files via upload

---
+++ IS-Lab2/IS_lab2.m
@@ -0,0 +1,49 @@
+clear;
+clc;
+% Duomenu mokymo paruosimas 
+x = 0.1:1/22:1;
+d = (1 + 0.6*sin(2*pi*x/0.7)) + 0.3*sin(2*pi*x)/2;
+
+% Susigeneruojam rand vertes pagal isejimus w1, w2 and b
+w1_1 = randn(1);
+w2_1 = randn(1);
+w3_1 = randn(1);
+w4_1 = randn(1);
+
+w1_2 = randn(1);
+w2_2 = randn(1);
+w3_2 = randn(1);
+w4_2 = randn(1);
+
+b1_1 = randn(1);
+b2_1 = randn(1);
+b3_1 = randn(1);
+b4_1 = randn(1);
+
+b1_2 = randn(1);
+
+% Skaiciuojame pirmos sluoksnio isejimus (ja reiks 20 kartu prasukti)
+% for index 1:20
+%     v1.1=x(index)*w1.1+b1.1;
+%     v2.1=x(index)*w2.1+b2.1;
+%     v3.1=x(index)*w3.1+b3.1;
+%     v4.1=x(index)*w4.1+b4.1;
+    
+v1_1=x(1)*w1_1+b1_1;
+v2_1=x(1)*w2_1+b2_1;
+v3_1=x(1)*w3_1+b3_1;
+v4_1=x(1)*w4_1+b4_1;
+
+%Pritaikom aktyviaja funkcija
+y1_1=1/(1+exp(-v1_1));
+y2_1=1/(1+exp(-v2_1));
+y3_1=1/(1+exp(-v3_1));
+y4_1=1/(1+exp(-v4_1));
+
+%Skaiciuojam antro sluoksnio isejimus
+v=y1_1*w1_2+y2_1*w2_2+y3_1*w3_2+y4_1*w4_2;
+
+y=v;
+
+%Skaiciuojam klaida
+e=d(1)-y;
