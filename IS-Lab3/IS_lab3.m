bb8c187609900a2a8ce83dd47312be350a218aa9
20163741 <36729822+20163741@users.noreply.github.com>
2020-12-15 22:10:30 +0200

    Add files via upload

--- IS-Lab3/IS_lab3.m
+++ IS-Lab3/IS_lab3.m
@@ -5,12 +5,9 @@ out = sin(2*pi*x)/2; % output
 % desired output values are calculated using formula: 
 % y = (1 + 0.6 * sin (2 * pi * x / 0.7)) + 0.3 * sin (2 * pi * x)) / 2; 
 
-% - kuriamas neuronø tinklas turëtø "modeliuoti/imituoti ðios formulës elgesá" 
-
 w = [randn(1) randn(1) randn(1)];
 b = randn(1);
-
-nu = 0.1;
+num = 0.1;
 
 %centrø reikðmes c1 ir c2, spinduliø reikðmes r1 ir r2 parinkite rankiniu bûdu;
 c = [0.5 0.8]; %c1 ir c2
@@ -21,9 +18,9 @@ for e=1:100
    for n=1:20
     y = gauss(x(n),r(1),c(1))*w(1)+gauss(x(n),r(2),c(2))*w(2)+b;
     err = out(n) - y; 
-    b = b + nu*err;
-    w(1) = w(1) + nu*err*gauss(x(n),r(1),c(1));
-    w(2) = w(2) + nu*err*gauss(x(n),r(2),c(2));
+    b = b + num*err;
+    w(1) = w(1) + num*err*gauss(x(n),r(1),c(1));
+    w(2) = w(2) + num*err*gauss(x(n),r(2),c(2));
    end
    e = e - 1;
 end
@@ -31,13 +28,14 @@ end
 yy = gauss(x,r(1),c(1))*w(1)+gauss(x,r(2),c(2))*w(2)+b;
 
 figure
-plot(x,out,'red',x,yy,'blue'); 
+plot(x,out,'r',x,yy,'b'); 
 title('raudona kreive pries apmokymà, o mëlyna po apmokymo')
+legend('pries apmokyma', 'po apmokymo')
 grid on;
  
 %-------------------------------------------------------------------------------------
 function F = gauss(x,r,c)     
-                              %dvi spindulio tipo funkcijos 
-                              %(naudokite Gauso funkcijas: F = exp(-(x-c)^2/(2*r^2));
-F = exp(-(x-c).^2/(2*r.^2));
+                              
+F = exp(-(x-c).^2/(2*r.^2)); % two Gaussian radial basis functions: F = exp(-(x-c)^2/(2*r^2
 end
+
