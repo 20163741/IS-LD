02fa80509d7bda613ae70899a07e45751861aaa5
20163741 <36729822+20163741@users.noreply.github.com>
2020-12-15 22:17:33 +0200

    Add files via upload

--- IS-Lab3/IS_lab3.m
+++ IS-Lab3/IS_lab3.m
@@ -1,6 +1,10 @@
+
+clc;
+clear all;
+close all;
+
 x = [0.1:1/22:1]; %input values are generated using equation x = 0.1: 1/22: 1; 
-                  
-                  
+                                 
 out = sin(2*pi*x)/2; % output 
 % desired output values are calculated using formula: 
 % y = (1 + 0.6 * sin (2 * pi * x / 0.7)) + 0.3 * sin (2 * pi * x)) / 2; 
@@ -27,14 +31,14 @@ end
 yy = gauss(x,r(1),c(1))*w(1)+gauss(x,r(2),c(2))*w(2)+b;
 
 figure(1)
-plot(x,out,'r',x,yy,'b'); 
-title('raudona kreive pries apmokymà, o mëlyna po apmokymo')
-legend('pries apmokyma', 'po apmokymo')
+plot(x,out,'g',x,yy,'b'); 
+title('Pokycio grafikas panaudojant apmokyma')
+legend('Pries apmokyma', 'Po apmokymo')
 grid on;
+hold on;
  
-%-------------------------------------------------------------------------------------
-function F = gauss(x,r,c)     
-                              
-F = exp(-(x-c).^2/(2*r.^2)); % two Gaussian radial basis functions: F = exp(-(x-c)^2/(2*r^2
+
+function F = gauss(x,r,c)                                   
+    F = exp(-(x-c).^2/(2*r.^2)); % two Gaussian radial basis functions: F = exp(-(x-c)^2/(2*r^2
 end
 
